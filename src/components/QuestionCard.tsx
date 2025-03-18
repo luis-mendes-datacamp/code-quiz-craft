5c06d8dc8e2dc0203ef05a079b89f64a9d74f839
Patryk Szulczyk <patryk.szulczyk@datacamp.com>
2025-03-18 15:27:58 +0100

    Bring back dark background

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,3 +1,4 @@
+/** @jsxImportSource @emotion/react */
 import { useEffect, useState } from 'react';
 import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
